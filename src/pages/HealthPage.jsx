15d72d601e7471743fa93967ecc99565156e7dd0
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2023-07-06 17:14:14 +0900

    Feat:"카카오 맵 연동"

--- src/pages/HealthPage.jsx
+++ src/pages/HealthPage.jsx
@@ -1,19 +1,20 @@
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
 import { Box, Checkbox, FormControlLabel, FormGroup, Skeleton } from '@mui/material';
 
 import { PUBLIC_URL } from '../global_variables';
 import useHttpRequest from '../hook/use-http';
+import KakaoMap from '../components/KakaoMap';
+
 const HealthPage = () => {
   const { isLoading, sendGetRequest } = useHttpRequest();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const getPublicDataHandler = data => {
-      console.log(data.data);
-    };
-    sendGetRequest(`${PUBLIC_URL}/B490001/sjHptMcalPstateInfoService`, getPublicDataHandler);
-  }, []);
+  // useEffect(() => {
+  //   const getPublicDataHandler = data => {
+  //     console.log(data);
+  //   };
+  //   sendGetRequest(`${PUBLIC_URL}/B490001/sjHptMcalPstateInfoService/getSjJijeongHptChakgiList?serviceKey=QTM%2Bsk32UMfVLD9pw13UXm%2FIAaDOGy3I0zKkbgdbppFTEg95hFdZpOOkgoesQwT48dig8oY7f3R9PC3%2Fw%2Fm8KQ%3D%3D`, getPublicDataHandler);
+  // }, []);
 
   return (
     <>
@@ -24,9 +25,8 @@ const HealthPage = () => {
         <FormControlLabel control={<Checkbox />} label="산재재활기관관리정보" />
       </FormGroup>
       <Box height="900px" >
-        <Skeleton height="100%">
-
-        </Skeleton>
+        <KakaoMap/>
+        {/* <Skeleton width="100vw" height="100vh"></Skeleton> */}
       </Box>
     </>
   );
