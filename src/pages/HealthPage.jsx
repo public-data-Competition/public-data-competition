88780b1f3b8394ae2da640903da356772882f5b1
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2023-07-15 23:51:44 +0900

    Feat:"현재위치에 따른 산재병원 리스트 출력"

--- src/pages/HealthPage.jsx
+++ src/pages/HealthPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState  } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { Box, Checkbox, FormControlLabel, FormGroup, Skeleton } from '@mui/material';
 
@@ -11,8 +11,10 @@ const HealthPage = () => {
   const { isLoading, sendGetRequest } = useHttpRequest();
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
+  const [address,setAddress] = useState('');
   const navigate = useNavigate();
 
+  console.log(latitude, longitude)
   const getCurrentLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -39,9 +41,27 @@ const HealthPage = () => {
       const data = await response.json();
 
       if (data.status === 'OK' && data.results.length > 0) {
-        const address = data.results[0].formatted_address;
-        console.log('주소:', address);
-        // 여기에서 주소를 원하는 방식으로 출력하거나 상태(state)에 저장할 수 있습니다.
+        const addressComponents = data.results[0].address_components;
+        // const desiredAddress = address.split(' ').slice(2).join(' ');
+        let area;
+
+        for (const component of addressComponents) {
+          if (component.types.includes('administrative_area_level_2')) {
+            // '구' 단위가 있는 경우
+            if (component.types.includes('sublocality')) {
+              area = component.long_name;
+            } else {
+              area = component.short_name;
+            }
+            break;
+          } else if (component.types.includes('sublocality')) {
+            // '동' 단위인 경우
+            area = component.long_name;
+          }
+        }
+        setAddress(area);
+        console.log('주소:', area);
+
       } else {
         console.error('주소를 가져오지 못했습니다.');
       }
@@ -50,8 +70,35 @@ const HealthPage = () => {
     }
   }
 
+  // async function getReverseGeocoding(latitude, longitude) {
+  //   const clientId = 'uy1qdrrbs3'; // Naver Maps Reverse Geocoding API 클라이언트 아이디
+  //   const clientSecret = '7ESPTMUl2iGbHltoFDIyQkrhBNgfrbdfsC16iAoL'; // Naver Maps Reverse Geocoding API 클라이언트 시크릿
+  //   const url = `/map-reversegeocode/v2/gc?coords=${longitude},${latitude}&sourcecrs=epsg:4326&orders=legalcode,addr,admcode,roadaddr&output=json`;
+
+  //   try {
+  //     const response = await fetch(url, {
+  //       headers: {
+  //         'X-NCP-APIGW-API-KEY-ID': clientId,
+  //         'X-NCP-APIGW-API-KEY': clientSecret
+  //       }
+  //     });
+  //     const data = await response.json();
+
+  //     if (data.status === 'OK' && data.results.length > 0) {
+  //       const address = data.results
+  //       console.log('주소:', address);
+  //     } else {
+  //       console.error('주소를 가져오지 못했습니다.');
+  //     }
+  //   } catch (error) {
+  //     console.error('API 요청 중 오류가 발생했습니다:', error);
+  //   }
+  // }
+
+
+
   async function logJSONData() {
-    const response = await fetch(`${PUBLIC_URL}/B490001/sjHptMcalPstateInfoService/getSjJijeongHptChakgiList?serviceKey=QTM%2Bsk32UMfVLD9pw13UXm%2FIAaDOGy3I0zKkbgdbppFTEg95hFdZpOOkgoesQwT48dig8oY7f3R9PC3%2Fw%2Fm8KQ%3D%3D`)
+    const response = await fetch(`${PUBLIC_URL}/B490001/sjHptMcalPstateInfoService/getSjJijeongHptChakgiList?serviceKey=QTM%2Bsk32UMfVLD9pw13UXm%2FIAaDOGy3I0zKkbgdbppFTEg95hFdZpOOkgoesQwT48dig8oY7f3R9PC3%2Fw%2Fm8KQ%3D%3D&addr=${address}`)
       .then(response => response.text())
       .then(xmlData => {
         // 여기서 xmlData는 XML 형식의 문자열 데이터입니다.
@@ -78,14 +125,15 @@ const HealthPage = () => {
   }
 
   useEffect(() => {
-    // logJSONData()
+    logJSONData()
     getCurrentLocation();
-  }, []);
+  }, [address]);
 
   useEffect(() => {
     if (latitude && longitude) {
       console.log(latitude, longitude)
       getAddressFromCoordinates(latitude, longitude);
+      // getReverseGeocoding(latitude, longitude)
     }
   }, [latitude, longitude]);
 
