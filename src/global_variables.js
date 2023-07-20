cacc9aca95abe5dff2a5804927623e24f170e9fd
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2023-07-20 23:35:04 +0900

    Feat:"보안프로토콜로 수정"

--- src/global_variables.js
+++ src/global_variables.js
@@ -1,5 +1,5 @@
 export const BACKEND_BASE_URL = 'http://localhost:8080';
-export const PUBLIC_URL = 'http://apis.data.go.kr';
-export const ADDRESS_URL = 'http://api.vworld.kr';
+export const PUBLIC_URL = 'https://apis.data.go.kr';
+export const ADDRESS_URL = 'https://api.vworld.kr';
 export const SERVICE_KEY = 'QTM%2Bsk32UMfVLD9pw13UXm%2FIAaDOGy3I0zKkbgdbppFTEg95hFdZpOOkgoesQwT48dig8oY7f3R9PC3%2Fw%2Fm8KQ%3D%3D'; //공공데이터 서비스키
 export const GEO_SERVICE_KEY = 'C0E8C230-562F-3336-B025-C8D7DD58689D'; //국토부 서비스키
